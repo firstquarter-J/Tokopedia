bc73427938576533ea3615b8d9286934d33c134b
SangHyun <>
2022-01-05 17:36:48 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+// HTTPS 인증 폴더 ~?
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.render('Hello world')
+})
+
+// Tokopedia Webhook Test
+app.post('/listener', (req, res) => res.send('Hello, World!'));
+
+app.post('/listener2', (req, res) => {
+    res.send(req.body.message);
+})
+
+// HTTPS 경로 설정 ~?
+const fs = require('fs');
+const http=require("http");
+const https=require("https");
+
+const options = {
+  ca: fs.readFileSync('/etc/letsencrypt/live/firstquarter.shop/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/firstquarter.shop/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/firstquarter.shop/cert.pem')
+  };
+  http.createServer(app).listen(3000);
+  https.createServer(options, app).listen(443);
