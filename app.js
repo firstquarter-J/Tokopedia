ffed9950d7cdda8c3769369e6250dedb34935503
SangHyun <>
2022-01-05 17:50:00 +0900

    webhook test1

--- app.js
+++ app.js
@@ -3,21 +3,6 @@ const app = express()
 
 // HTTPS 인증 폴더 ~?
 app.use(express.static('public'));
-
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
-
-// Tokopedia Webhook Test
-app.post('/listener', (req, res) => res.send('Hello, World!'));
-
-app.post('/listener2', (req, res) => {
-    res.send(req.body.message);
-})
-
 // HTTPS 경로 설정 ~?
 const fs = require('fs');
 const http=require("http");
@@ -30,3 +15,16 @@ const options = {
   };
   http.createServer(app).listen(3000);
   https.createServer(options, app).listen(443);
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+// Tokopedia Webhook Test
+
+app.post('/listener', (req, res) => {
+  res.send(req.body.message);
+})
