7e41fa06790574fb19f19a722d33f3742d2c958b
SangHyun <>
2022-01-05 17:44:56 +0900

    setting

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.render('Hello world')
+    res.send('Hello world')
 })
 
 // Tokopedia Webhook Test
