c44fe5d808afe7751449c2fad33fa11e745d4ac5
SangHyun <>
2022-01-05 19:40:08 +0900

    아몰랑 일단해봐

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 
-// HTTPS 인증 폴더 ~?
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('나랑 싸우자 토코피디아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ')
+})
+
+// LOCAL ONLY
+// const port = 3000;
+// app.listen(port, () => {
+//     console.log(`listening at http://localhost:${port}`)
+// })
+
+// HTTPS
 app.use(express.static('public'));
-// HTTPS 경로 설정 ~?
 const fs = require('fs');
 const http=require("http");
 const https=require("https");
@@ -16,15 +28,29 @@ const options = {
   http.createServer(app).listen(3000);
   https.createServer(options, app).listen(443);
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
-
 // Tokopedia Webhook Test
 
 app.post('/listener', (req, res) => {
   res.send(req.body.message);
 })
+
+const crypto = require('crypto')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+const YOUR_WEBHOOK_KEY = 'YOUR_SECRET'
+
+app.post('/listener', (req, res) => {
+  // Encrypt with SHA-256 and Encode to hexadecimal
+  let hmac = crypto.createHmac('sha256', YOUR_WEBHOOK_KEY)
+    .update(JSON.stringify(req.body))
+    .digest('hex')
+
+  // Compare our HMAC with your HMAC
+  if(!crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(req.get('Authorization-Hmac')))) {
+      console.log('Failed to verify!')
+      return
+  }
+  console.log('Successfully verified!')
+});
