e74a128943490db77802c80adde3f95bca2e50fd
Sahy <firstquarter88@gmail.com>
2022-01-07 14:56:40 +0900

    프로덕트 내놔

--- app.js
+++ app.js
@@ -82,7 +82,38 @@ app.post('/hi', (req, res) => {
   }
 })
 
-app.post('/listener', (req, res) => res.sendStatus(400));
+var https = require('follow-redirects').https;
+var fs = require('fs');
+
+var options = {
+  'method': 'GET',
+  'hostname': 'https://fs.tokopedia.net',
+  'path': '/inventory/v1/fs/15919/product/info?sku=k8k',
+  'headers': {
+    'Authorization': 'Bearer  c:TL8xZs91TeWwS9E859-_ow'
+  },
+  'maxRedirects': 20
+};
+
+var req = https.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+
+  res.on("error", function (error) {
+    console.error(error);
+  });
+});
+
+req.end();
+
 
 // const crypto = require('crypto')
 // const bodyParser = require('body-parser')
