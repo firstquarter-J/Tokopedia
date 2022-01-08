e0e70a23093869a69d3513d0a734f52214c149ad
Sahy <firstquarter88@gmail.com>
2022-01-09 07:55:15 +0900

    이게 뭐하는 짓이냐 ㅋㅋㅋㅋ

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const data = {
   name: "Sahy",
 }
 
-// Local HTTS or AWS Server HTTPS
+// Local HTTP or AWS Server HTTPS
 try {
   app.use(express.static('public'));
   const fs = require('fs');
@@ -26,26 +26,57 @@ try {
     };
     http.createServer(app).listen(3000);
     https.createServer(options, app).listen(443, () => {
-      console.log(`\x1b[1;36m안녕 서버~?\x1b[0m`)
+      console.log(
+        `\x1b[1;3;31m안
+  \x1b[0m\x1b[1;3;33m녕\x1b[0m
+    \x1b[1;3;32m난\x1b[0m
+      \x1b[1;3;36m서\x1b[0m
+        \x1b[1;3;34m버\x1b[0m
+          \x1b[1;3;35m야\x1b[0m
+      `)
     });
 } catch (err) {
   console.error(err)
   const port = 3000;
   app.listen(port, () => {
-      console.log(`\x1b[1;35m안녕 로컬 서버~~~\x1b[0m \x1b[1;36mhttp://localhost:${port}\x1b[0m`)
+      console.log(
+        `\x1b[1;3;31m안
+  \x1b[0m\x1b[1;3;33m녕\x1b[0m
+    \x1b[1;3;32m난\x1b[0m
+      \x1b[1;3;36m서\x1b[0m
+        \x1b[1;3;34m버\x1b[0m
+          \x1b[1;3;35m야\x1b[0m
+            \x1b[1;3;36mhttp://localhost:${port}\x1b[0m
+      `)
   })
 }
 
 app.get('/', (req, res) => {
   try {
-
-    const date = new Date(); 
-    const utcTime = date.getTime() + (date.getTimezoneOffset() * 60 * 1000); 
-    const utcTimeAddKoreaTime = 9 * 60 * 60 * 1000; 
-    const koreaTime = new Date(utcTime + utcTimeAddKoreaTime);
-    console.log(koreaTime)
-
-    console.log('\x1b[1;35m하잉~~~\x1b[0m', `\x1b[1;36m${new Date().toString()}\x1b[0m`);
+    console.log('\x1b[1;3;31m하\x1b[0m');
+    console.log('\x1b[1;3;33m  잉\x1b[0m');
+    console.log('\x1b[1;3;32m    지\x1b[0m');
+    console.log('\x1b[1;3;36m      금\x1b[0m');
+    console.log('\x1b[1;3;34m        은\x1b[0m');
+    console.log('\x1b[1;3;35m          몇\x1b[0m');
+    console.log('\x1b[1;3;30m            시~?\x1b[0m');
+    console.log('\x1b[1;3;36m                \x1b[0m', `\x1b[1;36m${new Date()}\x1b[0m`);
+    // console.log('\x1b[30m30= 검은색\x1b[0m');
+    // console.log('\x1b[1;30m30 bold = 회색\x1b[0m');
+    // console.log('\x1b[31m31 = 빨간색\x1b[0m');
+    // console.log('\x1b[1;31m31 bold = 밝은 빨간색\x1b[0m');
+    // console.log('\x1b[32m32 = 초록색\x1b[0m');
+    // console.log('\x1b[1;32m32 bold = 밝은 초록색\x1b[0m');
+    // console.log('\x1b[33m33\x1b[0m');
+    // console.log('\x1b[1;33m33\x1b[0m');
+    // console.log('\x1b[34m34\x1b[0m');
+    // console.log('\x1b[1;34m34\x1b[0m');
+    // console.log('\x1b[35m35\x1b[0m');
+    // console.log('\x1b[1;35m35\x1b[0m');
+    // console.log('\x1b[36m36\x1b[0m');
+    // console.log('\x1b[1;36m36\x1b[0m');
+    // console.log('\x1b[37m37\x1b[0m');
+    // console.log('\x1b[1;37m37\x1b[0m');
 
     res.render("index", data)
 
