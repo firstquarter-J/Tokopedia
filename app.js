e1c24c3f4f3ab8a7413327e430d2d04fa3a16ede
Sahy <firstquarter88@gmail.com>
2022-01-11 17:06:50 +0900

    adsdasdsadasd

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ app.post('/hi', (req, res) => {
     console.log(`\x1b[1;31m---절취선---\x1b[0m`, `\x1b[1;36m${new Date()}\x1b[0m`);
 
     // console.log(req.headers.authorization-Hmac) 미쳤나!!! 이거 없잖아????
-    console.log(req.headers.authorization-hmac)
-    console.log(req.get('Authorization-Hmac'))
+    // console.log(req.headers.authorization-hmac)
     console.log(req.get('Authorization-hmac'))
+    console.log(req.get('Authorization-Hmac'))
 
     // Encrypt with SHA-256 and Encode to hexadecimal
     let hmac = crypto.createHmac('sha256', YOUR_WEBHOOK_KEY)
